e09b3423e78fb50a503e5c2c6267b32dafbca578
Stas1494 <stason.1494@yandex.ru>
2022-12-12 18:33:31 +0500

    interface для props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Card, {CardVariant} from "./components/Card";
 
 const App = () => {
     return (
         <div>
-          Работает
+            <Card height='200px' width='200px' variant={CardVariant.outlined}>
+                <h2>Title</h2>
+                <div>Content Content Content Cont entC o nten tCon tent Con t ent Content</div>
+                <button>Buy</button>
+            </Card>
         </div>
     );
 };
